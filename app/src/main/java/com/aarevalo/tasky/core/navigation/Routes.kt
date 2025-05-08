dcc4ed26947215bbf31d8148c0b095e1df20800d
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2025-05-07 22:13:36 -0700

    Created Navigation Routes

---
+++ app/src/main/java/com/aarevalo/tasky/core/navigation/Routes.kt
@@ -0,0 +1,20 @@
+package com.aarevalo.tasky.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object SplashScreenRoute
+@Serializable
+object RegistrationScreenRoute
+@Serializable
+object LoginScreenRoute
+@Serializable
+object AgendaScreenRoute
+@Serializable
+object EventDetailScreenRoute
+@Serializable
+object TaskDetailScreenRoute
+@Serializable
+object ReminderDetailScreenRoute
+@Serializable
+object EditTextScreen
