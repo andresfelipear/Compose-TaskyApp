58664aa64384ecc5e20d99543d6c89600cc6532a
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2025-05-14 11:26:59 -0700

    Fixed LoginViewModel to avoid race conditions with surviving process death for only crucial data (isLogged)

--- app/src/main/java/com/aarevalo/tasky/auth/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/aarevalo/tasky/auth/presentation/login/LoginViewModel.kt
@@ -6,12 +6,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aarevalo.tasky.auth.domain.util.InputValidator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -21,44 +27,74 @@ class LoginViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
+    companion object {
+        private const val KEY_IS_LOGGED = "isLogged"
+    }
 
-    val state = savedStateHandle.getStateFlow(
-        "state",
-        LoginScreenState()
+    private val isLogged = savedStateHandle.getStateFlow(
+        KEY_IS_LOGGED,
+        false
     )
-        .onStart {
-            observePasswordText()
-        }
-        .stateIn(
+
+    private val _state = MutableStateFlow(LoginScreenState())
+
+    val state: StateFlow<LoginScreenState> = combine(
+        isLogged,
+        _state
+    ){ isLogged, state ->
+        LoginScreenState(
+            isLoggedIn = isLogged,
+            email = state.email,
+            passwordState = state.passwordState,
+            errorMessage = state.errorMessage,
+            isPasswordVisible = state.isPasswordVisible,
+            isValidEmail = state.isValidEmail,
+        )
+    }.onStart {
+        observePasswordText()
+    }
+    .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = LoginScreenState()
-        )
+    )
 
     fun onAction(action: LoginScreenAction) {
         when(action) {
             is LoginScreenAction.OnEmailChanged -> {
                 val validationResult = inputValidator.isValidEmailPattern(action.email)
-                savedStateHandle["state"] = state.value.copy(
-                    email = action.email,
-                    isValidEmail = validationResult
-                )
+                _state.update {
+                    it.copy(
+                        email = action.email,
+                        isValidEmail = validationResult
+                    )
+                }
             }
 
             is LoginScreenAction.OnPasswordVisibilityChanged -> {
-                savedStateHandle["state"] = state.value.copy(
-                    isPasswordVisible = !state.value.isPasswordVisible
-                )
+                _state.update {
+                    it.copy(isPasswordVisible = !action.isPasswordVisible)
+                }
             }
 
             is LoginScreenAction.OnLogin -> {
-                TODO()
+                _state.update {
+                    it.copy(isLoading = true)
+                }
+                //Simulate login process
+                viewModelScope.launch {
+                    delay(2000)
+                    _state.update {
+                        it.copy(isLoading = false)
+                    }
+                    savedStateHandle[KEY_IS_LOGGED] = true
+                }
             }
 
             is LoginScreenAction.OnErrorMessageSeen -> {
-                savedStateHandle["state"] = state.value.copy(
-                    errorMessage = null
-                )
+                _state.update {
+                    it.copy(errorMessage = null)
+                }
             }
 
             else -> Unit
@@ -69,9 +105,9 @@ class LoginViewModel @Inject constructor(
         snapshotFlow { state.value.passwordState.text }.distinctUntilChanged()
             .onEach { password ->
                 val validationResult = inputValidator.isValidPassword(password.toString())
-                savedStateHandle["state"] = state.value.copy(
-                    isValidPassword = validationResult.isValid
-                )
+                _state.update {
+                    it.copy(isValidPassword = validationResult.isValid)
+                }
             }
             .launchIn(viewModelScope)
     }
