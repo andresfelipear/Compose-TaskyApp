0dcd764219cfab69f6e9642f1d6c6cf07d2a4221
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2025-05-17 21:55:44 -0700

    Validating if the user is already authenticated loading initial data from the MainViewModel.

--- app/src/main/java/com/aarevalo/tasky/core/presentation/MainViewModel.kt
+++ app/src/main/java/com/aarevalo/tasky/core/presentation/MainViewModel.kt
@@ -2,25 +2,49 @@ package com.aarevalo.tasky.core.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.aarevalo.tasky.core.domain.preferences.SessionStorage
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel: ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val sessionStorage: SessionStorage
+): ViewModel() {
 
     private val _isAuthenticated = MutableStateFlow(false)
-    val isAuthenticated = _isAuthenticated.asStateFlow()
+    val isAuthenticated = _isAuthenticated
+        .onStart {
+            checkAuth()
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = false
+        )
 
     private val _isCheckingAuth = MutableStateFlow(true)
     val isCheckingAuth = _isCheckingAuth.asStateFlow()
 
-    init {
+    private fun checkAuth() {
         viewModelScope.launch {
-            delay(3000)
-            _isCheckingAuth.value = false
-            _isAuthenticated.value = false
+            _isCheckingAuth.update {
+                true
+            }
+            delay(2000)
+            _isAuthenticated.update {
+                sessionStorage.getSession() != null
+            }
+            _isCheckingAuth.update {
+                false
+            }
         }
     }
-
 }
