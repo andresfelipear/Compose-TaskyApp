fa0a9917805c39798d32dbf8f2d44aca3876f535
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2025-05-21 00:46:05 -0700

    created AgendaScreenHeader component. Added icon ic_calendar_today

--- app/src/main/java/com/aarevalo/tasky/agenda/presentation/components/AgendaScreenHeader.kt
+++ app/src/main/java/com/aarevalo/tasky/agenda/presentation/components/AgendaScreenHeader.kt
@@ -1,30 +1,33 @@
 package com.aarevalo.tasky.agenda.presentation.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.aarevalo.tasky.R
 import com.aarevalo.tasky.ui.theme.LocalSpacing
 import com.aarevalo.tasky.ui.theme.TaskyTheme
+import java.time.LocalDate
 
 @Composable
 fun AgendaScreenHeader(
     month: String,
-    fullname: String,
+    initials: String,
     modifier: Modifier = Modifier,
     onOpenCalendar: () -> Unit = {},
 ) {
@@ -33,19 +36,20 @@ fun AgendaScreenHeader(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(top = 30.dp)
+            .background(color = MaterialTheme.colorScheme.primary)
             .padding(
                 horizontal = spacing.spaceMedium,
                 vertical = spacing.spaceSmall
             ),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Column(
+        Row(
             modifier = Modifier,
-            verticalArrangement = Arrangement.spacedBy(4.dp)
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                text = month,
+                text = month.uppercase(),
                 style = MaterialTheme.typography.labelMedium.copy(
                     color = MaterialTheme.colorScheme.onPrimary
                 ),
@@ -61,31 +65,37 @@ fun AgendaScreenHeader(
             }
         }
 
-        Column(
+        Row(
             modifier = Modifier,
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             IconButton(
                 onClick = { onOpenCalendar() }
             ) {
                 Icon(
-                    imageVector = Icons.Default.CalendarToday,
+                    painter = painterResource(id = R.drawable.ic_calendar_today),
                     contentDescription = "arrow down",
-                    tint = MaterialTheme.colorScheme.onPrimary
+                    tint = MaterialTheme.colorScheme.onPrimary,
                 )
             }
+
+            AvatarIcon(
+                initials = initials,
+            )
         }
 
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 @Preview(showBackground = true, apiLevel = 34)
 fun AgendaScreenHeaderPreview() {
     TaskyTheme {
         AgendaScreenHeader(
-            month = "January",
-            fullname = "John Doe",
+            month = LocalDate.now().month.toString(),
+            initials = "jd",
         )
     }
 }
